a5cd310bccc068020cb3a660627b1b0b07da2973
pil <44938522+hanu9257@users.noreply.github.com>
2022-06-05 03:40:25 +0900

    Test : Split using stack

--- btree.cpp
+++ btree.cpp
@@ -12,10 +12,19 @@ void btree::insert(char *key, uint64_t val)
 {
 	// Please implement this function in project 2.
 	page *current_page = root;
-	page *stack[height];
-	int path_count = 0;
-	while (current_page->get_type() == INTERNAL)
+	page *stack[height]; // TODO split 발생 시 스택 사용해서 삽입을 위해 지나왔던 부모 page에 key 삽입해줘야함. 
+	page *new_page = nullptr;
+	char *medium_key;
+	char *input_key;
+	uint64_t input_value;
+	int path_count = 1;
+
+	/* 현재 k를 insert할 때 split이 발생했음에도 부모 page의 key가 insert되지 않았음. */
+
+	while (current_page->get_type() == INTERNAL) /* traversing */
 	{
+		stack[path_count] = current_page;
+		path_count++;
 		page *next_page = (page *)current_page->find(key);
 		if(next_page == nullptr)
 		{
@@ -23,19 +32,29 @@ void btree::insert(char *key, uint64_t val)
 		}
 		current_page = next_page;
 	}
-	char *medium_key;
-	page *new_page = nullptr;
-	if (current_page->insert(key, val) == false)
+
+	while(path_count != 0)
 	{
-		new_page = current_page->split(key, val, &medium_key);
-		if (current_page == root)
+		if (current_page->insert(key, val) == false) /* leaf */
 		{
-			page *new_root = new page(INTERNAL);
-			new_root->set_leftmost_ptr(root);
-			new_root->insert(medium_key, (uint64_t)new_page);
-			root = new_root;
-			height++;
+			new_page = current_page->split(key, val, &medium_key);
+			if (current_page == root)
+			{
+				page *new_root = new page(INTERNAL);
+				new_root->set_leftmost_ptr(root);
+				new_root->insert(medium_key, (uint64_t)new_page);
+				root = new_root;
+				height++;
+			}
 		}
+
+		if(new_page != nullptr) /* after split */
+		{
+			current_page = stack[path_count];
+			current_page->insert(medium_key, (uint64_t)new_page);
+			new_page = nullptr;
+		}
+		path_count--;
 	}
 }
 
